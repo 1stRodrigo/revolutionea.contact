45da660b7e8ce63d585afecd27f08ac7d564b5cf
Rodrigo oliveira <98712381+1stRodrigo@users.noreply.github.com>
2022-08-30 23:11:06 -0300

    Add files via upload

---
+++ scripts/whats.js
@@ -0,0 +1,11 @@
+document.querySelector('#submit').addEventListener('click', function() {
+    let cliente = document.querySelector('#nomefull').value;
+    let telefone = document.querySelector('#telefone').value;
+    let endereco = document.querySelector('#address').value;
+    let services = document.querySelector('#services_select').value;
+    
+   let url = "https://api.whatsapp.com/send?phone=5519987164384&text=Bem vindo a Revolution -->> %0A Qual é o seu nome? %0A " + cliente + " %0A Qual o seu telefone ?%0A" + 
+   telefone + "%0A Qual o seu endereço %0A "+ endereco +" %0A Quais serviços você precisa? %0A "+ services +""; 
+   
+   window.open(url);
+});
