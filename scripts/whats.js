df1e08c8714467c8a04fc18091e5aab19afeb7f1
Rodrigo oliveira <98712381+1stRodrigo@users.noreply.github.com>
2022-09-20 08:41:11 -0300

    Update whats.js

--- scripts/whats.js
+++ scripts/whats.js
@@ -1,3 +1,4 @@
+
 document.querySelector('#submit').addEventListener('touchstart', function() {
     let cliente = document.querySelector('#nomefull').value;
     let telefone = document.querySelector('#telefone').value;
@@ -17,9 +18,10 @@ document.querySelector('#submit').addEventListener('click', function() {
     let endereco = document.querySelector('#endereço').value;
     var services = document.querySelector('#services_select');
     var opcoes = [...services.selectedOptions].map(Option => Option.text);
+    console.log(opcoes)
     
    let url = "https://api.whatsapp.com/send?phone=5519987164384&text=Bem vindo a Revolution -->> %0A Qual é o seu nome? %0A " + cliente + " %0A Qual o seu telefone ?%0A" + 
-   telefone + "%0A Qual o seu endereço %0A " + endereco + " %0A Quais serviços você precisa? %0A "+ opcoes +""; 
+   telefone + "%0A Qual o seu endereço? %0A " + endereco + " %0A Quais serviços você precisa? %0A%0D%0D "+ opcoes +""; 
    
    window.open(url);
 });
