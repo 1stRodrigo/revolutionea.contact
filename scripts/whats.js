04a6348cb2151ed5272e46599966eab5c16b3fc8
Rodrigo oliveira <98712381+1stRodrigo@users.noreply.github.com>
2022-09-21 14:28:42 -0300

    Update whats.js

--- scripts/whats.js
+++ scripts/whats.js
@@ -4,10 +4,20 @@ document.querySelector('#submit').addEventListener('touchstart', function() {
     let telefone = document.querySelector('#telefone').value;
     let endereco = document.querySelector('#endereço').value;
     let services = document.querySelector('#services_select').value;
-    var opcoes = [...services.selectedOptions].map(Option => Option.text);
+    //var opcoes = [...services.selectedOptions].map(Option => Option.text);
+    const valueOpcoes = Array.from(services.selectedOptions).map(e => e.text);
+
+    const breakLineOpcoes = [
+        valueOpcoes[0] += ' %0A%0A ',
+        valueOpcoes[1] += ' %0A%0A ',
+        valueOpcoes[2] += ' %0A%0A ', 
+    ]
+    document.getElementById('seção').innerHTML = breakLineOpcoes;
     
+   /*let url = "https://api.whatsapp.com/send?phone=5519987164384&text=Bem vindo a Revolution -->> %0A Qual é o seu nome? %0A " + cliente + " %0A Qual o seu telefone ?%0A" + 
+   telefone + "%0A Qual o seu endereço %0A "+ endereco +" %0A Quais serviços você precisa? %0A "+ opcoes +""; */
    let url = "https://api.whatsapp.com/send?phone=5519987164384&text=Bem vindo a Revolution -->> %0A Qual é o seu nome? %0A " + cliente + " %0A Qual o seu telefone ?%0A" + 
-   telefone + "%0A Qual o seu endereço %0A "+ endereco +" %0A Quais serviços você precisa? %0A "+ opcoes +""; 
+   telefone + "%0A Qual o seu endereço? %0A " + endereco + " %0A Quais serviços você precisa? %0A%0A %0A %0A "+ breakLineOpcoes +" %0A ";
    
    window.open(url);
 });
