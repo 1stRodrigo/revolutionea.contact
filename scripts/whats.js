09b120e863b8409972aa94d11d86e8e10dd1c4f1
Rodrigo oliveira <98712381+1stRodrigo@users.noreply.github.com>
2022-09-27 14:41:56 -0300

    Update whats.js

--- scripts/whats.js
+++ scripts/whats.js
@@ -22,44 +22,82 @@ document.querySelector('#submit').addEventListener('touchstart', function() {
    window.open(url);
 });
 
-document.querySelector('#submit').addEventListener('click', function() {
+
+const fields = document.querySelectorAll("[required]")
+
+function customValidation(event){
+    const field = event.target
+
+    // lógica para verificar se existem erros
+    function verifyErrors() {
+        let foundError = false;
+
+        for(let error in field.validity) {
+            // se não for customError
+            // então verifica se tem erro
+            if (error != "customError" && field.validity[error]) {
+                foundError = true
+            }
+
+        }
+        return foundError;
+    }
+
+    const error = verifyErrors()
+    console.log("Error exists: ", error)
+
+    if (error) {
+        //trocar mensagem de required
+        field.setCustomValidity("Esse campo é obrigatório")
+    } else {
+        field.setCustomValidity("") 
+    }
+}
+
+
+
+for ( field of fields) {
+    field.addEventListener("invalid", customValidation)
+}
+
+
+
+
+
+
+
+
+    document.querySelector("form")
+    .addEventListener("submit", event => {
+        console.log("enviar o formulário")
+
+
+        sendMessage();
+        // não vai enviar o formulário
+        //event.preventDefault()
+    } )
+
+document.querySelector('#submit').addEventListener('click', sendMessage());
+
+function sendMessage() {
     let cliente = document.querySelector('#nomefull').value;
     let telefone = document.querySelector('#telefone').value;
     let endereco = document.querySelector('#endereço').value;
     var services = document.querySelector('#services_select');
     //var opcoes = [...services.selectedOptions].map(Option => Option.text);
     const textOpcoes = Array.from(services.selectedOptions).map(e => e.text);
-    /*if (textOpcoes[0].lenght > 1) {
-        textOpcoes[0] += ' %0A ';
-    }
-    if (textOpcoes[1] == Text ) {
-        textOpcoes[1] += ' %0A ';
-    }
-    if (textOpcoes[2] == Text ) {
-        textOpcoes[2] += ' %0A ';
-    }*/
 
     const breakLineOpcoes =  textOpcoes
-    /*valueOpcoes[0].innerText += valueOpcoes[0] +  ` "%0A" `
-    valueOpcoes[1].innerText += valueOpcoes[1] +  ` "%0A" `
-    valueOpcoes[2].innerText += valueOpcoes[2] +  ` "%0A" ` */
 
-    //TALVEZ SEJA POR ESSE CAMINHO 
-    /*const breakLineOpcoes = [
-        valueOpcoes[0] += ' %0A%0A ',
-        valueOpcoes[1] += ' %0A%0A ',
-        valueOpcoes[2] += ' %0A%0A ', 
-    ]
-    */
-    document.getElementById('seção').innerHTML = breakLineOpcoes;
-    //console.log(valueOpcoes[0], valueOpcoes[1], valueOpcoes[2]);
+    /*if
+        let url = "https://api.whatsapp.com/send?phone=5519987164384&text=_Olá, seja bem vindo a *Revolution EA*_ %0A%0ANós agradecemos seu contato *" + cliente + "*! %0A%0ARelizar um bom serviço à você é a nossa prioridade. Iremos te retornar o quanto antes!%0A---------------------------------------%0A Confira os dados abaixo: %0A---------------------------------------%0A%0A*Telefone:* %0A" + telefone + "%0A%0A*Endereço:*%0A" + endereco + " %0A%0A*Serviços:*%0A" + breakLineOpcoes + ". %0A";
+    }*/
+
     
-
-    /*var valueOpcoes = Array.from(services.options).map(e => e.value);
-    document.getElementById('seção').innerHTML = valueOpcoes;*/
-    //var opcoes = [...services.selectedOptions].map(Option => Option.text);
+    //document.getElementById('seção').innerHTML = breakLineOpcoes;
     
     let url = "https://api.whatsapp.com/send?phone=5519987164384&text=_Olá, seja bem vindo a *Revolution EA*_ %0A%0ANós agradecemos seu contato *" + cliente + "*! %0A%0ARelizar um bom serviço à você é a nossa prioridade. Iremos te retornar o quanto antes!%0A---------------------------------------%0A Confira os dados abaixo: %0A---------------------------------------%0A%0A*Telefone:* %0A" + telefone + "%0A%0A*Endereço:*%0A" + endereco + " %0A%0A*Serviços:*%0A" + breakLineOpcoes + ". %0A";
+
    
    window.open(url);
-});
+};
