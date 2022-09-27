d6f20f3b0df4f53a55e38bd81e66b73793bcdfce
Rodrigo oliveira <98712381+1stRodrigo@users.noreply.github.com>
2022-09-27 20:52:13 -0300

    Update whats.js

--- scripts/whats.js
+++ scripts/whats.js
@@ -1,27 +1,4 @@
-
-document.querySelector('#submit').addEventListener('touchstart', function() {
-    let cliente = document.querySelector('#nomefull').value;
-    let telefone = document.querySelector('#telefone').value;
-    let endereco = document.querySelector('#endereço').value;
-    let services = document.querySelector('#services_select').value;
-    //var opcoes = [...services.selectedOptions].map(Option => Option.text);
-    const valueOpcoes = Array.from(services.selectedOptions).map(e => e.text);
-
-    /*const breakLineOpcoes = [
-        valueOpcoes[0] += ' %0A%0A ',
-        valueOpcoes[1] += ' %0A%0A ',
-        valueOpcoes[2] += ' %0A%0A ', 
-    ]*/
-    const breakLineOpcoes = valueOpcoes
-    document.getElementById('seção').innerHTML = breakLineOpcoes;
-    
-   /*let url = "https://api.whatsapp.com/send?phone=5519987164384&text=Bem vindo a Revolution -->> %0A Qual é o seu nome? %0A " + cliente + " %0A Qual o seu telefone ?%0A" + 
-   telefone + "%0A Qual o seu endereço %0A "+ endereco +" %0A Quais serviços você precisa? %0A "+ opcoes +""; */
-   let url = "https://api.whatsapp.com/send?phone=5519987164384&text=_Olá, seja bem vindo a *Revolution EA*_ %0A%0ANós agradecemos seu contato *" + cliente + "*! %0A%0ARelizar um bom serviço à você é a nossa prioridade. Iremos te retornar o quanto antes!%0A---------------------------------------%0A Confira os dados abaixo: %0A---------------------------------------%0A%0A*Telefone:* %0A" + telefone + "%0A%0A*Endereço:*%0A" + endereco + " %0A%0A*Serviços:*%0A" + breakLineOpcoes + ". %0A";
-
-   window.open(url);
-});
-
+//VALIDAÇÕES DO FORMULÁRIO
 
 const fields = document.querySelectorAll("[required]")
 
@@ -55,29 +32,27 @@ function customValidation(event){
 }
 
 
-
 for ( field of fields) {
     field.addEventListener("invalid", customValidation)
 }
 
-
-
-
-
-
-
-
+//ENVIAR FORMULÁRIO
     document.querySelector("form")
     .addEventListener("submit", event => {
         console.log("enviar o formulário")
+        sendMessage()
+        
+        /*
+        document.querySelector('#submit').addEventListener('click', sendMessage()),
+        document.querySelector('#submit').addEventListener('touchstart', sendMessage());*/
 
 
-        sendMessage();
         // não vai enviar o formulário
-        //event.preventDefault()
+        // event.preventDefault()
     } )
 
-document.querySelector('#submit').addEventListener('click', sendMessage());
+
+
 
 function sendMessage() {
     let cliente = document.querySelector('#nomefull').value;
@@ -88,13 +63,8 @@ function sendMessage() {
     const textOpcoes = Array.from(services.selectedOptions).map(e => e.text);
 
     const breakLineOpcoes =  textOpcoes
-
-    /*if
-        let url = "https://api.whatsapp.com/send?phone=5519987164384&text=_Olá, seja bem vindo a *Revolution EA*_ %0A%0ANós agradecemos seu contato *" + cliente + "*! %0A%0ARelizar um bom serviço à você é a nossa prioridade. Iremos te retornar o quanto antes!%0A---------------------------------------%0A Confira os dados abaixo: %0A---------------------------------------%0A%0A*Telefone:* %0A" + telefone + "%0A%0A*Endereço:*%0A" + endereco + " %0A%0A*Serviços:*%0A" + breakLineOpcoes + ". %0A";
-    }*/
-
     
-    //document.getElementById('seção').innerHTML = breakLineOpcoes;
+    console.log("Executando função sendMessage")
     
     let url = "https://api.whatsapp.com/send?phone=5519987164384&text=_Olá, seja bem vindo a *Revolution EA*_ %0A%0ANós agradecemos seu contato *" + cliente + "*! %0A%0ARelizar um bom serviço à você é a nossa prioridade. Iremos te retornar o quanto antes!%0A---------------------------------------%0A Confira os dados abaixo: %0A---------------------------------------%0A%0A*Telefone:* %0A" + telefone + "%0A%0A*Endereço:*%0A" + endereco + " %0A%0A*Serviços:*%0A" + breakLineOpcoes + ". %0A";
 
